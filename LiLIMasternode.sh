5f545853db3aba39c91ad2e682e7ff35a8c6f447
Timothy St Amant <timmyamant@gmail.com>
2018-03-21 18:30:11 -0400

    udpate

--- LiLIMasternode.sh
+++ LiLIMasternode.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-echo ***************************************************
-echo *** SETTING UP SWAP FILE FOR vps ON 1gb ram only **
-echo ***************************************************
+
+
+apt-get update
+apt-get install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
+apt-get install libboost-all-dev software-properties-common -y
+add-apt-repository ppa:bitcoin/bitcoin -y
+apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev -y
+
+
+cd 
+
+apt-get autoremove -y 
 
 
 cd /
@@ -11,22 +20,7 @@ sudo swapon swapfile
 sudo nano etc/fstab
 cat /proc/meminfo
 
-echo ************************************************************
-echo *** installing files that are needed for LILI MN to work  **
-echo ************************************************************
 
-
-apt-get update
-apt-get install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
-apt-get install libboost-all-dev software-properties-common -y
-add-apt-repository ppa:bitcoin/bitcoin -y
-apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev -y
-
-cd 
-
-echo *******************************************
-echo *** Installing and settingup MN for LILI **
-echo *******************************************
 
 git clone https://github.com/jembem/lili
 cd lili
