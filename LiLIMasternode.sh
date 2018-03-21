d241a6cba7dc6ceaafb08bf5e6a4d5de2f905357
Timothy St Amant <timmyamant@gmail.com>
2018-03-21 18:03:25 -0400

    update

--- LiLIMasternode.sh
+++ LiLIMasternode.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 apt-get update
-apt-get install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev
-apt-get install libboost-all-dev software-properties-common
+apt-get install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
+apt-get install libboost-all-dev software-properties-common -y
 add-apt-repository ppa:bitcoin/bitcoin
-apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev
+apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev -y
+
+cd
+
+git clone https://github.com/jembem/lili
+cd lili
+./autogen.sh
+./configure
+make
+
