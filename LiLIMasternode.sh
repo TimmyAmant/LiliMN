b2cadba90d7c307da385d62c3d7d5e3da82320f3
Timothy St Amant <timmyamant@gmail.com>
2018-03-21 19:56:17 -0400

    update

--- LiLIMasternode.sh
+++ LiLIMasternode.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-apt-get update
 apt-get install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
 apt-get install libboost-all-dev software-properties-common -y
 add-apt-repository ppa:bitcoin/bitcoin -y
 apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev -y
+apt-get install nano -y
 apt-get update -y
 apt-get upgrade -y
 
@@ -15,8 +15,8 @@ apt-get autoremove -y
 
 
 cd /
-sudo dd if=/dev/zero of=swapfile bs=1M count=3000 -y
-sudo mkswap swapfile -y
+sudo dd if=/dev/zero of=swapfile bs=1M count=3000
+sudo mkswap swapfile
 sudo swapon swapfile
 sudo nano etc/fstab
 cat /proc/meminfo
@@ -29,3 +29,10 @@ cd lili
 ./configure
 make
 
+cd /
+
+nano /root/.lili/lili.conf
+
+cd /
+cd lili/src/
+/lilid
