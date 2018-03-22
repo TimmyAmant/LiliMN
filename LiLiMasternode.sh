9354e10f41633957c62281a1fa3161d5f590f909
Timothy St Amant <timmyamant@gmail.com>
2018-03-21 20:12:13 -0400

    add sleep

--- LiLiMasternode.sh
+++ LiLiMasternode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+sleep 10
 apt-get install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
 apt-get install libboost-all-dev software-properties-common -y
 add-apt-repository ppa:bitcoin/bitcoin -y
@@ -8,31 +8,42 @@ apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev -
 apt-get install nano -y
 apt-get update -y
 apt-get upgrade -y
+sleep 10
+cd
 
-cd 
+apt-get autoremove -y
 
-apt-get autoremove -y 
-
-
+sleep 10
 cd /
 sudo dd if=/dev/zero of=swapfile bs=1M count=3000
+sleep 10
 sudo mkswap swapfile
+sleep 10
 sudo swapon swapfile
+sleep 10
 sudo nano etc/fstab
+sleep 10
 cat /proc/meminfo
+sleep 10
 
 
 
 git clone https://github.com/jembem/lili
+sleep 10
 cd lili
+sleep 10
 ./autogen.sh
+sleep 10
 ./configure
+sleep 10
 make
-
+sleep 10
 cd /
-
+sleep 10
 nano /root/.lili/lili.conf
-
+sleep 10
 cd /
+sleep 10
 cd lili/src/
+sleep 10
 ./lilid
